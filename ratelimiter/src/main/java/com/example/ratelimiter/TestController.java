7cde5512782b164e1e79813d328e18c080f88775
nislamovs <nizamiislamovs@gmail.com>
2021-08-02 11:38:06 +0300

    Resilience4j rate limited example.

---
+++ ratelimiter/src/main/java/com/example/ratelimiter/TestController.java
@@ -0,0 +1,33 @@
+package com.example.ratelimiter;
+
+
+import static org.springframework.http.ResponseEntity.ok;
+
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1")
+public class TestController {
+
+  @GetMapping("/test1")
+  @RateLimiter(name = "service1", fallbackMethod = "fallbackZZ")
+  public ResponseEntity test() {
+    System.out.println("In testRateLimiter");
+    return ok("Test ok!");
+  }
+
+  public ResponseEntity fallbackZZ(Throwable t) {
+    System.out.println("in fallback");
+    return new ResponseEntity("Too much requests!", HttpStatus.SERVICE_UNAVAILABLE);
+  }
+
+}
---
+++ ratelimiter/src/main/resources/makeRequest.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+curl -XGET http://localhost:8080/api/v1/test1
+curl -XGET http://localhost:8080/api/v1/test1
+curl -XGET http://localhost:8080/api/v1/test1
+curl -XGET http://localhost:8080/api/v1/test1
+curl -XGET http://localhost:8080/api/v1/test1
+curl -XGET http://localhost:8080/api/v1/test1
+curl -XGET http://localhost:8080/api/v1/test1
+curl -XGET http://localhost:8080/api/v1/test1
+curl -XGET http://localhost:8080/api/v1/test1
+curl -XGET http://localhost:8080/api/v1/test1
